77b8c961d2da83d70b48b7155bab21e41ff787da
MarshallYuan <Marshall.yuan@foxmail.com>
2015-04-13 17:33:29 +0800

    重构屏幕适配参数,编写竖版主界面

--- src/com/yuchengtech/yuchengo2o/activity/impl/LoginActivity.java
+++ src/com/yuchengtech/yuchengo2o/activity/impl/LoginActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.yuchengtech.yuchengo2o.R;
 import com.yuchengtech.yuchengo2o.activity.BaseActivity;
@@ -17,6 +21,19 @@ import com.yuchengtech.yuchengo2o.activity.BaseActivity;
  */
 public class LoginActivity extends BaseActivity implements OnClickListener {
 
+	@ViewInject(R.id.et_company)
+	private EditText et_company;// 公司编号
+	@ViewInject(R.id.et_account)
+	private EditText et_account;// 账户编号
+	@ViewInject(R.id.et_password)
+	private EditText et_password;// 密码
+	@ViewInject(R.id.cb_save_account)
+	private CheckBox cb_save_account;// 保存账号
+	@ViewInject(R.id.cb_auto_logins)
+	private CheckBox cb_auto_login;// 自动登录
+	@ViewInject(R.id.btn_login)
+	private Button btn_login;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,6 +42,16 @@ public class LoginActivity extends BaseActivity implements OnClickListener {
 	}
 
 	@Override
+	@OnClick(R.id.btn_login)
 	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_login:
+			// 点击了登陆按钮
+			Intent enterHomeIntent = new Intent(this, MainActivity.class);
+			startActivity(enterHomeIntent);
+			break;
+		default:
+			break;
+		}
 	}
 }
